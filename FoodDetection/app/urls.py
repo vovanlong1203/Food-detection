9c5a4cbd28b70672b9a99591b5fe54301472f3f6
ncgiang1102 <giangnc1@viit.com.vn>
2023-12-12 19:36:01 +0700

    st1

--- FoodDetection/app/urls.py
+++ FoodDetection/app/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from app import views
 
-urlpatterns = [path("", views.home, name="home")]
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("upload/", views.upload_image, name="upload_image"),
+    path("index/", views.index, name="index"),
+]
--- FoodDetection/app/views.py
+++ FoodDetection/app/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from django.http import HttpResponse
+from .models import UploadedImage
 
 
 # Create your views here.
 def home(request):
     return render(request, "index.html")
+
+
+def upload_image(request):
+    if request.method == "POST" and request.FILES.get("image"):
+        image = request.FILES["image"]
+        UploadedImage.objects.create(image=image)
+        return redirect("index")
+    return redirect("index")
+
+
+def index(request):
+    images = UploadedImage.objects.latest("timestamp")
+    return render(request, "index.html", {"images": images})
